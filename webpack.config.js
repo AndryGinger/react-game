5bf2d4da27c70dc54f95e9415134a8aaeabf0ddf
Andrey Marhalyk <aom@anadeainc.com>
2021-02-21 19:38:41 +0300

    Add webpack with dev dependencies

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require("path");
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const ESLintPlugin = require("eslint-webpack-plugin");
+
+module.exports = {
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: path.resolve(__dirname, "dist"),
+    port: 9000,
+    open: true,
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        loader: "file-loader",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin(),
+    new ESLintPlugin(),
+    new MiniCssExtractPlugin(),
+  ],
+};
