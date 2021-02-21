4e694e3232b882ab5476068c45e7ee88551be6c5
Andrey Marhalyk <aom@anadeainc.com>
2021-02-21 22:03:47 +0300

    Add board builder

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Board from "../Board";
 
 export default function App() {
-  return <Board />;
+  const [boardSize, updateBoardSize] = useState(3);
+
+  return (
+    <>
+      <h1 style={{ textAlign: "center" }}>Hexagon</h1>
+      <input
+        type="number"
+        onChange={(event) => updateBoardSize(parseInt(event.target.value))}
+        min="3"
+        max="7"
+        value={boardSize}
+      />
+      <Board boardSize={boardSize} />
+    </>
+  );
 }
 
 module.hot.accept();
--- src/Board/Board.jsx
+++ src/Board/Board.jsx
@@ -1,21 +1,15 @@
 import React from "react";
 import Hex from "../Hex";
 import HexRow from "../HexRow";
+import boardBuilder from "../utilities/boardBuilder";
 
 import * as S from "./styled";
 
-const Board = () => {
-  const board = [
-    [0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0],
-  ];
+const Board = ({ boardSize }) => {
+  const board = boardBuilder(boardSize);
 
   return (
     <>
-      <h1>Hexagon</h1>
       <S.BoardWrapper>
         {board.map((hexes, index) => (
           <HexRow hexes={hexes} key={index} />
