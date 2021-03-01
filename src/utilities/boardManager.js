b5ffff4708c719ef4d935e9ed4ada8dad91b78e0
Andrey Marhalyk <aom@anadeainc.com>
2021-03-02 01:53:07 +0300

    Adjust enemy bot

--- src/utilities/boardManager.js
+++ src/utilities/boardManager.js
@@ -47,35 +47,79 @@ export const getCellsAround = ({
 };
 
 export const detectEnemyMove = ({ board }) => {
-  let cells = [];
-  let origin;
+  let originsWithCells = {},
+    availableCells,
+    originX,
+    originY,
+    originsWithAttack = {};
 
-  board.forEach((_, y) => {
-    board.forEach((_, x) => {
+  board.forEach((row, y) => {
+    board.forEach((el, x) => {
       if (board[y][x] === playersIndicators.enemy) {
-        cells.push([y, x]);
+        const cellsAround = getCellsAround({
+          x,
+          y,
+          board,
+          cellValue: playersIndicators.none,
+          withJump: true
+        });
+
+        if (cellsAround.length) {
+          originsWithCells[[y, x]] = _.uniqBy(cellsAround, (cell) =>
+            JSON.stringify(cell)
+          );
+        }
       }
     });
   });
 
-  cells = _.shuffle(cells);
+  Object.keys(originsWithCells).forEach((key) => {
+    originsWithCells[key].forEach((cell) => {
+      const playerCellsAround = getCellsAround({
+        x: cell[1],
+        y: cell[0],
+        board,
+        cellValue: playersIndicators.player,
+        withJump: true
+      });
 
-  cells.some((cell) => {
-    let cellsAround = getCellsAround({
-      x: cell[1],
-      y: cell[0],
-      board,
-      cellValue: playersIndicators.none,
-      withJump: true
+      if (playerCellsAround.length) {
+        originsWithAttack[key] = [cell];
+      }
     });
-
-    if (cellsAround.length) {
-      origin = cell;
-      cells = cellsAround;
-    }
-
-    return origin !== undefined;
   });
 
-  return { origin, availableCells: cells };
+  const sourceHash = Object.keys(originsWithAttack).length
+    ? originsWithAttack
+    : originsWithCells;
+  const originsCells = Object.keys(sourceHash);
+
+  [originY, originX] = _.shuffle(originsCells)[0].split(",");
+
+  const origin = [parseInt(originY), parseInt(originX)];
+  availableCells = sourceHash[origin];
+
+  if (availableCells.length > 0) {
+    let steps = [];
+    let jumps = [];
+
+    availableCells.forEach((cell) => {
+      let enemyCells = getCellsAround({
+        x: cell[1],
+        y: cell[0],
+        board,
+        cellValue: playersIndicators.enemy
+      });
+
+      enemyCells.some(
+        (enemyCell) => origin[0] === enemyCell[0] && origin[1] === enemyCell[1]
+      )
+        ? steps.push(cell)
+        : jumps.push(cell);
+    });
+
+    availableCells = steps.length ? steps : jumps;
+  }
+
+  return { origin, availableCells };
 };
