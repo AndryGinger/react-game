66010e5249b3a55f46d7e99526a30bca45b03e53
Andrey Marhalyk <aom@anadeainc.com>
2021-03-03 01:17:28 +0300

    Animate PlayAgain component

--- src/PlayAgain/styled.jsx
+++ src/PlayAgain/styled.jsx
@@ -8,6 +8,26 @@ export const ModalWrapper = styled(Modal)`
   left: 50%;
   transform: translate(-50%, -20%);
   outline: none;
+
+  -webkit-animation: fadeIn 1s;
+  animation: fadeIn 2s;
+
+  @-webkit-keyframes fadeIn {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+  @keyframes fadeIn {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
 `;
 
 export const ScoreWrapper = styled.div`
