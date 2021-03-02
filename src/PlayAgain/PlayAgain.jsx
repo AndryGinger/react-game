3801de20bbab67fe3ece8d256d0f67f7764164d5
Andrey Marhalyk <aom@anadeainc.com>
2021-03-03 01:08:09 +0300

    Add score

--- src/PlayAgain/PlayAgain.jsx
+++ src/PlayAgain/PlayAgain.jsx
@@ -5,17 +5,30 @@ import { func } from "prop-types";
 import * as S from "./styled";
 
 const PlayAgain = ({ updateIsGameStarted }) => {
-  const { isGameOver, updateIsGameOver } = useGame();
+  const { isGameOver, updateIsGameOver, playersCells } = useGame();
 
   const handleClick = () => {
     updateIsGameStarted(false);
     updateIsGameOver(false);
   };
 
+  const gameStatus =
+    (playersCells.player === playersCells.enemy && "Draw") ||
+    (playersCells.player > playersCells.enemy ? "You Win" : "You Lose");
+
   return (
     <>
       {isGameOver && (
         <S.ModalWrapper isOpen>
+          <S.ScoreWrapper>
+            <S.ScoreHex />
+            <S.Score>{playersCells.player}</S.Score>
+          </S.ScoreWrapper>
+          <S.ScoreWrapper enemy>
+            <S.ScoreHex enemy />
+            <S.Score enemy>{playersCells.enemy}</S.Score>
+          </S.ScoreWrapper>
+          <S.GameStatus gameStatus={gameStatus}>{gameStatus}</S.GameStatus>
           <S.RetryButton onClick={handleClick}>Play Again</S.RetryButton>
         </S.ModalWrapper>
       )}
