a218bfd3e48c57796956f2c78bcc69bfdb930cfd
Andrey Marhalyk <aom@anadeainc.com>
2021-03-03 20:38:26 +0300

    Add footer

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import Board from "../Board";
 import GameMenu from "../GameMenu";
 import PlayAgain from "../PlayAgain";
+import GithubLogo from "../assets/images/github_logo.png";
+import RsSchoolLogo from "../assets/images/rs_school_js.svg";
 import { GameProvider } from "../contexts/GameContext";
 
 import * as S from "./styled";
@@ -30,6 +32,15 @@ export default function App() {
             )}
           </GameProvider>
         )}
+        <S.Footer>
+          <S.FooterLink href="https://rs.school/js" target="_blank">
+            <S.Logo src={RsSchoolLogo} />
+          </S.FooterLink>
+          <S.FooterText>2021</S.FooterText>
+          <S.FooterLink href="https://github.com/AndryGinger" target="_blank">
+            <S.Logo src={GithubLogo} />
+          </S.FooterLink>
+        </S.Footer>
       </S.GameWrapper>
     </>
   );
--- src/App/styled.jsx
+++ src/App/styled.jsx
@@ -24,3 +24,32 @@ export const GlobalStyle = createGlobalStyle`
     height: 100%;
   }
 `;
+
+export const Footer = styled.div`
+  z-index: 9999;
+  position: absolute;
+  bottom: 15px;
+  left: 0;
+  width: 100%;
+  text-align: center;
+`;
+
+export const FooterLink = styled.a`
+  display: inline-block;
+  background: #fff;
+  border-radius: 5px;
+  padding: 5px;
+  width: 100px;
+  height: 30px;
+`;
+
+export const Logo = styled.img`
+  width: 100%;
+  height: 100%;
+`;
+
+export const FooterText = styled.span`
+  color: #fff;
+  font-size: 28px;
+  margin: 0 20%;
+`;
